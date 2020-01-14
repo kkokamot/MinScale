2bee5b660343bf578c434ccc4b33927ceed60784
Kristina Okamoto <kkokamot@ucsc.edu>
2020-01-14 11:37:25 -0800

    Remove some comments

--- FindMinScale.m
+++ FindMinScale.m
@@ -1,10 +1,8 @@
 %%Calculate point of parallel-perp. divergence
 %data should be cut to only include resolvable data before running function
 
-%H1 must be the perpendicular spectra and H2 should be the parallel spectra
 function[div_x, div_y] = FindMinScale(L1, nH1, L2, nH2)
 
-%determine longer dataset
 %determine which is perpendicular and which is parallel
 if nH1(end) <= nH2(end)
     H1 = nH1;
@@ -13,12 +11,10 @@ else
     H1 = nH2;
     H2 = nH1;
 end
-    
 
 shortL = min(length(L1),length(L2));
 for i = 5:shortL
     j = shortL+1-i;
-    %j=i;
     if H2(j) <= 1.1236 *H1(j)
         div_x = L1(j);
         div_y = H1(j);
